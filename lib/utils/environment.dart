e5a12a12b46208455aec6c21e08f2a1a9206648f
cristiansavino <savinocristian89@gmail.com>
2021-07-12 12:10:16 +0100

    add real url

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
   static String apiUrl = 'https://a9f2fef8d603.ngrok.io/api';
   static String socketUrl = 'https://a9f2fef8d603.ngrok.io';
-  // static String apiUrl = 'https://cristiansavino.com/api';
-  // static String socketUrl = 'https://cristiansavino.com';
+  // static String apiUrl = 'https://fulbito.app/api';
+  // static String socketUrl = 'https://fulbito.app';
   static String googlePlaceApiKey = 'AIzaSyCIW7l5SSIkF7JS1LOQAi6Rcsmm_10yJTQ';
 }

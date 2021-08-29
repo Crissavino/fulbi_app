8c939ccddf15c2ebfb87161775bbbc003226b54f
cristiansavino <savinocristian89@gmail.com>
2021-08-29 09:35:05 +0100

    add preview to links in chats

--- lib/utils/environment.dart
+++ lib/utils/environment.dart
@@ -1,8 +1,8 @@
 class EnvironmentConstants {
-  static String apiUrl = 'https://22aac7d42330.ngrok.io/api';
-  static String socketUrl = 'https://22aac7d42330.ngrok.io';
-  // static String apiUrl = 'https://fulbito.app/api';
-  // static String socketUrl = 'https://fulbito.app';
+  // static String apiUrl = 'https://22aac7d42330.ngrok.io/api';
+  // static String socketUrl = 'https://22aac7d42330.ngrok.io';
+  static String apiUrl = 'https://fulbito.app/api';
+  static String socketUrl = 'https://fulbito.app';
   static String mapBoxApiKey = 'pk.eyJ1IjoiY3Jpc3NhdmlubyIsImEiOiJja3JhejN4bHQxMWF2MnpwODA0dGFlemNzIn0.qWasazYjV_FaM7yD6pWAEw';
   //google key path = /Users/cristiansavino_1/upload-keystore.jks 89!
 }
